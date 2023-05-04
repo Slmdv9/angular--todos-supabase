fdf33932ebd1bc2b0c9139c28282c3731e79b160
Samuel <samuel9lmdv@gmail.com>
2023-05-04 09:06:44 +0200

    CRUD methods to API Service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient, } from '@supabase/supabase-js';
+
+import { environment } from 'src/environments/environment.prod';
+import { Todo } from './todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(environment.supabase.url, environment.supabase.key);
+   }
+
+  async addTodo(todo: Todo) {
+    const { data, error } = await 
+      this.supabase.from('todosDefault')
+      .insert(todo)
+      return {data, error };
+  }
+
+  async getTodos() {
+    let { data: todos, error } = await this.supabase
+      .from('todosDefault')
+      .select('*')
+      .limit(10)
+    return { todos, error };
+  }
+
+  async deleteTodo(id: string) {
+    const data = await this.supabase
+      .from('todosDefault')
+      .delete()
+      .match( {id: id })
+    return data;
+  }
+
+  async update(todo: Todo) {
+    const { data, error } = await this.supabase
+      .from('todosDefault')
+      .update('todosDefault')
+      .match({ id: todo.id });
+  }
 }
